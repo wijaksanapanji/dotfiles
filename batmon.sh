fc290e8dd38ea69dc3ea9e7e3c31b78226f6f58a
Panji Wijaksana <wijaksanapanji6@gmail.com>
2019-03-16 17:16:00 +0800

    loss of track

--- batmon.sh
+++ batmon.sh
@@ -5,7 +5,7 @@ do
     batteryLevel=$(acpi -b | grep -P -o '[0-9]+(?=%)')
     if [ $batteryLevel -le 15 ] && [ $isCharging -eq 0 ]
     then
-        notify-send -u critical "Battery low" "Battery level is ${batteryLevel}%"
+        notify-send -i ~/.config/dunst/icons/battery.png -u critical "Battery low" "Battery level is ${batteryLevel}%"
         paplay /home/docky/.sounds/blop.wav
     fi
     sleep 180
--- greeting_nofif.sh
+++ greeting_nofif.sh
@@ -2,15 +2,15 @@
 jam=`date +"%H"`
 if [ $jam -le 12 ]
 then
-    notify-send -t 10000 "Holla~" "Good morning docky"
+    notify-send -i ~/.config/dunst/icons/sunset.png -t 10000 "Holla~" "Good morning docky"
     paplay /home/docky/.sounds/blop.wav
 elif [ $jam -ge 12 ] && [ $jam -le 18 ]
 then
-    notify-send -t 10000 "Holla~" "Good afternoon docky"
+    notify-send -i ~/.config/dunst/icons/sun.png -t 10000 "Holla~" "Good afternoon docky"
     paplay /home/docky/.sounds/blop.wav
 elif [ $jam -ge 18 ]
 then
-    notify-send -t 10000 "Holla~" "Good evening docky"
+    notify-send -i ~/.config/dunst/icons/moon.png -t 10000 "Holla~" "Good evening docky"
     paplay /home/docky/.sounds/blop.wav
 fi
 
